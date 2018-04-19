46a1af24d214f0368494c3272b4ea9498b7a70ea
Minnie Buenaventura <minnie_2234@yahoo.com>
2018-04-19 18:21:03 -0400

    Initial commit

---
+++ app/src/main/java/minnie/gallaudet/edu/minnegallaudet/Bison.java
@@ -0,0 +1,35 @@
+package minnie.gallaudet.edu.minnegallaudet;
+
+
+public class Bison {
+    private int mX;
+    private int mY;
+    private int mVelocity;
+
+    public void setVelocity(int velocity){
+        mVelocity = velocity;
+    }
+    public int getVelocity(){
+        return mVelocity;
+    }
+
+    public void setX(int x){
+        mX = x;
+    }
+    public void setY(int y){
+        mY = y;
+    }
+    public int getX(){
+        return mX;
+    }
+    public int getY(){
+        return mY;
+    }
+    public void move(int destinationX, int destinationY){
+        int distX = destinationX - mX;
+        int distY = destinationY -  mY;
+        mX += distX / mVelocity;
+        mY += distY / mVelocity;
+    }
+
+}
---
+++ app/src/main/java/minnie/gallaudet/edu/minnegallaudet/Seal.java
@@ -0,0 +1,19 @@
+package minnie.gallaudet.edu.minnegallaudet;
+
+public class Seal {
+    private int mX;
+    private int mY;
+
+    public int getX(){
+        return mX;
+    }
+    public void setX(int x){
+        mX = x;
+    }
+    public int getY(){
+        return mY;
+    }
+    public void setY(int y){
+        mY = y;
+    }
+}
